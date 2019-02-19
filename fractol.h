83c3fa564d06aeac4631e97f048ae187340edea5
gerus66 <gerus66@gmail.com>
2019-02-19 17:03:00 +0300

    add constant for Mandelbrot, Scale

--- fractol.h
+++ fractol.h
@@ -6,7 +6,7 @@
 /*   By: mbartole <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/15 15:55:54 by mbartole          #+#    #+#             */
-/*   Updated: 2019/02/18 14:32:06 by mbartole         ###   ########.fr       */
+/*   Updated: 2019/02/19 15:20:07 by mbartole         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,17 +49,27 @@ typedef struct	s_quebox
 typedef struct	s_kernbox
 {
 	cl_kernel			kern;
-	cl_mem				map_buf;
-	cl_mem				f_buf;
 	int					*map;
+	cl_mem				map_buf;
+	float				f[3];
+	cl_mem				f_buf;
 }				t_kernbox;
 
 # define IMG_SIZE 1000
 # define MAP_LEN 1000000
 # define WND_W 1300
 # define WND_H 1000
+# define HELP_W 300
+
+# define SC_COEF 2
+
 # define PROGRAM_FILE "create_map.cl"
+
 # define MAND "mandelbrot"
+# define MAND_START_X -2.0
+# define MAND_START_Y 2.0
+# define MAND_START_SC 0.004
+
 # define ER_MAL "cant malloc it!\n"
 # define ER_MLX "cannot initialize mlx\n"
 # define ER_WND "cannot create new window\n"
@@ -68,8 +78,8 @@ typedef struct	s_kernbox
 
 void			init_queue(t_quebox *qbox);
 void			init_kern(t_kernbox *kbox, char *kern_name, t_quebox *qbox);
-void			evolve_kern(t_kernbox *kbox, t_quebox *qbox, float *param);
+void			evolve_kern(t_kernbox *kbox, t_quebox *qbox);
 void			clean_all(t_kernbox *kbox, t_quebox *qbox, char *msg);
-void			paint_it(int *map, t_imgbox *ibox);
+void			reprint_all(t_kernbox *kbox, t_quebox *qbox, t_imgbox *ibox);
 
 #endif
